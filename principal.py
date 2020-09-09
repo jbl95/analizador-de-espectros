08145d839e0aafd79c5b44487dde5334379473f7
jbl95 <lauriajuanbrian@gmail.com>
2020-09-09 01:45:36 -0300

    2nd-commit

--- principal.py
+++ principal.py
@@ -21,16 +21,17 @@ import analizador as fxEsp
 
 vmax = 1
 dc = 0
-fs = 100
-N = 100
+fs = 500
+N = 500
 ph = npy.pi * 0
 f = 10
+
   
 tt,xx = fxSen.mi_func_sen(vmax,dc,f,ph,N,fs)  
-
+tt2,xx2 = fxSen.mi_func_sen(vmax/2,dc,3*f,ph,N,fs)  
 #ff = npy.linspace(0, (N-1)*fs/N, N)
 
-modulo,fase = fxEsp.mi_analizador(xx,fs,N)
+modulo,fase = fxEsp.mi_analizador(xx+xx2,fs,N)
 
 # Para chequear usando la función FFT
 
@@ -38,18 +39,19 @@ modulo,fase = fxEsp.mi_analizador(xx,fs,N)
 # modulo = npy.abs(fft)
 # fase = npy.angle(fft)
 
+tt = npy.arange(N/fs, step=1/fs)
+
 plt.figure(1)
-ploteo1 = plt.plot(fs*tt,modulo)
+plt.subplot(121)
+ploteo1 = plt.plot(fs*tt[:N // 2],modulo[:N // 2])
 plt.title('Modulo')
-plt.xlabel('Frecuencia')
+plt.xlabel('Frecuencia(Hz)')
 plt.ylabel('Amplitud')
 plt.grid()
-plt.show()
-
-plt.figure(2)
-ploteo1 = plt.plot(fs*tt,fase)
+plt.subplot(122)
+ploteo1 = plt.plot(fs*tt[:N // 2],fase[:N // 2])
 plt.title('Fase')
-plt.xlabel('Frecuencia')
+plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Fase')
 plt.grid()
 plt.show()
